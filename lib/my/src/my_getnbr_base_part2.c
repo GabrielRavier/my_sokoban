7a42f4735a00f956958236b64fdca5d7e56f34b3
Gabriel Ravier <gabravier@gmail.com>
2020-10-07 15:45:05 +0200

    Corrected one formatting error
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- lib/my/src/my_getnbr_base_part2.c
+++ lib/my/src/my_getnbr_base_part2.c
@@ -7,7 +7,9 @@
 
 #include "my.h"
 
-// Checks whether there is a valid digit at *digit_ptr and gets it into digit_ptr, with the base being the contents of base. The return value determines whether this function was successful at finding a digit
+// Checks whether there is a valid digit at *digit_ptr and gets it into
+// digit_ptr, with the base being the contents of base. The return value
+// determines whether this function was successful at finding a digit
 bool my_find_digit_from_base(
     const char *digit_character_ptr, const char *base, unsigned char *digit_ptr)
 {

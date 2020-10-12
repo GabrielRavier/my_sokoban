627d7cca4ba8f4db0a19284ebaec38da214b3eba
Gabriel Ravier <gabravier@gmail.com>
2020-10-12 14:25:25 +0200

    Did task 04 (my_advanced_sort_word_array) and made a bunchj of changes to accomodate it
    Changes:
    - Added my_qsort_r to libmy to facilitate implementation of my_advanced_sort_word_array
    - Reimplemented my_qsort as a wrapper for my_qsort_r
    - Implemented my_advanced_sort_word_array with a noice wrapper around my_qsort_r
    - Moved calculate_word_table_elements into word_array_utils.c as calculate_word_table_size
    - Moved my_sort_word_array.h to sort_word_array_funcs.h and added my_advanced_sort_word_array to it
    - Added tests for my_advanced_sort_word_array
    
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- lib/my/src/my_qsort.c
+++ lib/my/src/my_qsort.c
@@ -2,50 +2,22 @@
 ** EPITECH PROJECT, 2020
 ** libmy
 ** File description:
-** Sorts num_elements of size element_size in ascending order as decided by
+** Sorts an array with num_elements of element_size size, according to
 ** comparison_function
 */
 
 #include "my.h"
 
-static void swap_elements(char *elem1, char *elem2, size_t element_size)
+static int qsort_r_helper_for_qsort(
+    const void *elem1, const void *elem2, void *comparison_function_ptr)
 {
-    char tmp;
-
-    do {
-        tmp = *elem1;
-        *elem1++ = *elem2;
-        *elem2++ = tmp;
-    } while (--element_size != 0);
-}
-
-static char *find_smallest_element(char *first, char *last, size_t element_size,
-    int (*comparison_function)(const void *, const void *))
-{
-    char *smallest;
-    if (first == last)
-        return last;
-
-    smallest = first;
-    first += element_size;
-
-    for (; first != last; first += element_size) {
-        if (comparison_function(first, smallest) < 0)
-            smallest = first;
-    }
-
-    return smallest;
+    return (*((int (**)(const void *, const void *))comparison_function_ptr))(
+        elem1, elem2);
 }
 
 void my_qsort(void *base, size_t num_elements, size_t element_size,
     int (*comparison_function)(const void *, const void *))
 {
-    char *first = (char *)base;
-    char *last = first + (num_elements * element_size);
-
-    for (char *it = first; it != last; it += element_size) {
-        char *smallest =
-            find_smallest_element(it, last, element_size, comparison_function);
-        swap_elements(smallest, it, element_size);
-    }
+    my_qsort_r(base, num_elements, element_size, &qsort_r_helper_for_qsort,
+        (void *)&comparison_function);
 }
---
+++ lib/my/src/my_qsort_r.c
@@ -0,0 +1,53 @@
+/*
+** EPITECH PROJECT, 2020
+** libmy
+** File description:
+** Sorts num_elements of size element_size in ascending order as decided by
+** comparison_function, with the possibility of passing extra data to it
+*/
+
+#include "my.h"
+
+static void swap_elements(char *elem1, char *elem2, size_t element_size)
+{
+    char tmp;
+
+    do {
+        tmp = *elem1;
+        *elem1++ = *elem2;
+        *elem2++ = tmp;
+    } while (--element_size != 0);
+}
+
+static char *find_smallest_element(char *first, char *last, size_t element_size,
+    int (*comparison_function)(const void *, const void *, void *),
+    void *argument)
+{
+    char *smallest;
+    if (first == last)
+        return last;
+
+    smallest = first;
+    first += element_size;
+
+    for (; first != last; first += element_size) {
+        if (comparison_function(first, smallest, argument) < 0)
+            smallest = first;
+    }
+
+    return smallest;
+}
+
+void my_qsort_r(void *base, size_t num_elements, size_t element_size,
+    int (*comparison_function)(const void *, const void *, void *),
+    void *argument)
+{
+    char *first = (char *)base;
+    char *last = first + (num_elements * element_size);
+
+    for (char *it = first; it != last; it += element_size) {
+        char *smallest = find_smallest_element(
+            it, last, element_size, comparison_function, argument);
+        swap_elements(smallest, it, element_size);
+    }
+}

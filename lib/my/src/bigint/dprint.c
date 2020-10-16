95090f0e886586539b767e65caa9a5706a1df29d
Gabriel Ravier <gabravier@gmail.com>
2020-10-17 00:43:32 +0200

    libmy: Do error handling in bigint/dprint
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- lib/my/src/bigint/dprint.c
+++ lib/my/src/bigint/dprint.c
@@ -8,13 +8,18 @@
 #include "my_bigint.h"
 #include "my.h"
 #include <unistd.h>
+#include <stdio.h>
 
-void my_bigint_dprint(int fd, const struct my_bigint *num)
+bool my_bigint_dprint(int fd, const struct my_bigint *num)
 {
     if (num->is_negative)
-        my_dputc('-', fd);
+        if (my_dputc('-', fd) == EOF)
+            return false;
 
     for (size_t i = num->num_digits; i != 0; --i) {
-        my_dputc('0' + num->number[i - 1], fd);
+        if (my_dputc('0' + num->number[i - 1], fd) == EOF)
+            return false;
     }
+
+    return true;
 }

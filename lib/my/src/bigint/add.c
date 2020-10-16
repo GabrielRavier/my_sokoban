10055e256edf21151ae86b29dfd6d0a19f1c56af
Gabriel Ravier <gabravier@gmail.com>
2020-10-16 14:19:18 +0200

    Fix the case where we're doing `a + -a` (i.e. different signs, same absolute)
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- lib/my/src/bigint/add.c
+++ lib/my/src/bigint/add.c
@@ -10,11 +10,16 @@
 void my_bigint_add(struct my_bigint *result, const struct my_bigint *operand2)
 {
     bool is_result_negative;
+    int compare_result;
     if (result->is_negative == operand2->is_negative)
         my_bigint_add_unsigned(result, operand2);
     else {
-        is_result_negative = my_bigint_compare_unsigned(result, operand2) > 0 ?
-            result->is_negative : operand2->is_negative;
+        compare_result = my_bigint_compare_unsigned(result, operand2);
+        if (compare_result == 0)
+            is_result_negative = false;
+        else
+            is_result_negative = compare_result >= 0 ?
+                result->is_negative : operand2->is_negative;
         my_bigint_sub_unsigned(result, operand2);
         result->is_negative = is_result_negative;
     }

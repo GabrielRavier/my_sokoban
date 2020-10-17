f7a7357083dd2287b503f968f4c75f6aa3321045
Gabriel Ravier <gabravier@gmail.com>
2020-10-17 13:48:26 +0200

    Made it more clear that `character` is caster to `unsigned char`
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- lib/my/src/my_memset.c
+++ lib/my/src/my_memset.c
@@ -12,7 +12,7 @@ void *my_memset(void *destination, int character, size_t length)
     unsigned char *char_destination = (unsigned char *)destination;
 
     while (length--)
-        *char_destination++ = character;
+        *char_destination++ = (unsigned char)character;
 
     return destination;
 }

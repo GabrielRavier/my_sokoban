0abff39a316bbcfa93b109db19d1a30a2d4d8f55
Gabriel Ravier <gabravier@gmail.com>
2020-10-18 21:54:51 +0200

    Redid some formatting to be better
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- lib/my/include/my/stdlib.h
+++ lib/my/include/my/stdlib.h
@@ -16,6 +16,5 @@ void my_qsort(void *base, size_t num_elements, size_t element_size,
 void my_qsort_r(void *base, size_t num_elements, size_t element_size,
     int (*comparison_function)(const void *, const void *, void *),
     void *argument) MY_ATTR_NONNULL((1, 4));
-void *my_realloc_size(void *ptr, size_t new_length,
-    size_t original_length) MY_ATTR_WARN_UNUSED_RESULT MY_ATTR_NOTHROW
-    MY_ATTR_ALLOC_SIZE((2));
+void *my_realloc_size(void *ptr, size_t new_length, size_t original_length)
+    MY_ATTR_WARN_UNUSED_RESULT MY_ATTR_NOTHROW MY_ATTR_ALLOC_SIZE((2));

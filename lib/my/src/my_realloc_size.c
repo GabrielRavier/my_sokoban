725ecf3844f89b6047a1f057b905b3142fa4c4b5
Gabriel Ravier <gabravier@gmail.com>
2020-10-18 14:23:58 +0200

    libmy: Removed some else after returns that made the code less readable
    Also made it so that all variable declarations are at the beginning of the function
    
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- lib/my/src/my_realloc_size.c
+++ lib/my/src/my_realloc_size.c
@@ -11,16 +11,17 @@
 static void *handle_other_cases(void *ptr, size_t new_length,
     size_t original_length)
 {
+    void *new_ptr;
+
     if (new_length <= original_length)
         return ptr;
-    else {
-        void *new_ptr = malloc(new_length);
-        if (new_ptr) {
-            my_memcpy(new_ptr, ptr, original_length);
-            free(ptr);
-        }
-        return new_ptr;
+
+    new_ptr = malloc(new_length);
+    if (new_ptr) {
+        my_memcpy(new_ptr, ptr, original_length);
+        free(ptr);
     }
+    return new_ptr;
 }
 
 void *my_realloc_size(void *ptr, size_t new_length, size_t original_length)
@@ -28,8 +29,10 @@ void *my_realloc_size(void *ptr, size_t new_length, size_t original_length)
     if (new_length == 0) {
         free(ptr);
         return NULL;
-    } else if (ptr == NULL)
+    }
+
+    if (ptr == NULL)
         return malloc(new_length);
-    else
-        return handle_other_cases(ptr, new_length, original_length);
+
+    return handle_other_cases(ptr, new_length, original_length);
 }

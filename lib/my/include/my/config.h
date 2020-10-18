cfa24bb870e65f883b287dd67d87e4de368018df
Gabriel Ravier <gabravier@gmail.com>
2020-10-18 21:36:17 +0200

    libmy: Used appropriate attributes for stdlib functions
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- lib/my/include/my/config.h
+++ lib/my/include/my/config.h
@@ -16,3 +16,5 @@
 #define MY_ATTR_WARN_UNUSED_RESULT MY_ATTRIBUTE((warn_unused_result))
 #define MY_ATTR_CONST MY_ATTRIBUTE((const))
 #define MY_ATTR_NOTHROW MY_ATTRIBUTE((nothrow))
+#define MY_ATTR_NONNULL(parameters) MY_ATTRIBUTE((nonnull parameters))
+#define MY_ATTR_ALLOC_SIZE(parameters) MY_ATTRIBUTE((alloc_size parameters))
--- lib/my/include/my/stdlib.h
+++ lib/my/include/my/stdlib.h
@@ -11,9 +11,11 @@
 #include <stddef.h>
 
 void my_qsort(void *base, size_t num_elements, size_t element_size,
-    int (*comparison_function)(const void *, const void *));
+    int (*comparison_function)(const void *, const void *))
+    MY_ATTR_NONNULL((1, 4));
 void my_qsort_r(void *base, size_t num_elements, size_t element_size,
     int (*comparison_function)(const void *, const void *, void *),
-    void *argument);
+    void *argument) MY_ATTR_NONNULL((1, 4));
 void *my_realloc_size(void *ptr, size_t new_length,
-    size_t original_length) MY_ATTR_WARN_UNUSED_RESULT;
+    size_t original_length) MY_ATTR_WARN_UNUSED_RESULT MY_ATTR_NOTHROW
+    MY_ATTR_ALLOC_SIZE((2));

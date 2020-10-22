3d8911ac4fc31c543ab65349551763913cc58f82
Gabriel Ravier <gabravier@gmail.com>
2020-10-23 01:31:15 +0200

    Implemented my_string_erase in libmy to finish shunting yard implementation (and did so)
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

---
+++ lib/my/src/my_string/erase.c
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2020
+** libmy
+** File description:
+** Implements erase
+*/
+
+#include "my/my_string.h"
+#include "my/string.h"
+
+struct my_string *my_string_erase(
+    struct my_string *self, size_t index, size_t count)
+{
+    if (self == NULL || index > self->length || (index + count) > self->length)
+        return self;
+    my_memmove(self->string + index, self->string + index + count, self->length - (index + count));
+    self->length -= count;
+    self->string[self->length] = '\0';
+    return self;
+}

fdc51c22718f6a93f492bf3d607352b76d6d9d95
Gabriel Ravier <gabravier@gmail.com>
2020-10-23 11:04:34 +0200

    Corrected "too long line errors" and potential formatting problem
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- lib/my/src/my_string/erase.c
+++ lib/my/src/my_string/erase.c
@@ -13,7 +13,8 @@ struct my_string *my_string_erase(
 {
     if (self == NULL || index > self->length || (index + count) > self->length)
         return self;
-    my_memmove(self->string + index, self->string + index + count, self->length - (index + count));
+    my_memmove(self->string + index, self->string + index + count,
+        self->length - (index + count));
     self->length -= count;
     self->string[self->length] = '\0';
     return self;

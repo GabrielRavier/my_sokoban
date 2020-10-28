53bc555bbf76bb44fec640f0f72a0b9ea0a31212
Gabriel Ravier <gabravier@gmail.com>
2020-10-28 15:12:10 +0100

    Updated libmy Makefile and added modulo tests (they don't work yet, working on it...)
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

---
+++ tests/libmy/bigint/mod.c
@@ -0,0 +1,71 @@
+/*
+** EPITECH PROJECT, 2020
+** libmy
+** File description:
+** Tests bigint modulo
+*/
+
+#include "my/bigint.h"
+#include <criterion/criterion.h>
+#include <limits.h>
+
+static void do_test(int a, int b)
+{
+    if (b == 0)
+        return;
+    
+    struct my_bigint *bigint_a = my_bigint_new_from_int(a);
+    struct my_bigint *bigint_b = my_bigint_new_from_int(b);
+    struct my_bigint *bigint_result = my_bigint_new_from_int(a % b);
+    cr_assert_eq(my_bigint_compare(my_bigint_mod(bigint_a, bigint_b), bigint_result), 0);
+    my_bigint_free(bigint_a);
+    my_bigint_free(bigint_b);
+    my_bigint_free(bigint_result);
+}
+
+Test(my_bigint_mod, very_simple_tests)
+{
+    for (int i = -100; i < 100; ++i)
+        for (int j = -100; j < 100; ++j)
+            do_test(i, j);
+
+    for (int i = -10000000; i < (-10000000 + 100); ++i)
+        for (int j = -100; j < 100; ++j)
+            do_test(i, j);
+
+    for (int i = -100; i < 100; ++i)
+        for (int j = -10000000; j < (-10000000 + 100); ++j)
+            do_test(i, j);
+
+    for (int i = -10000000; i < (-10000000 + 100); ++i)
+        for (int j = -10000000; j < (-10000000 + 100); ++j)
+            do_test(i, j);
+
+    for (int i = 10000000; i > (10000000 - 100); --i)
+        for (int j = -100; j < 100; ++j)
+            do_test(i, j);
+
+    for (int i = 10000000; i > (10000000 - 100); --i)
+        for (int j = -10000000; j < (-10000000 + 100); ++j)
+            do_test(i, j);
+
+    for (int i = -100; i < 100; ++i)
+        for (int j = 10000000; j > (10000000 - 100); --j)
+            do_test(i, j);
+
+    for (int i = -10000000; i < (-10000000 + 100); ++i)
+        for (int j = 10000000; j > (10000000 - 100); --j)
+            do_test(i, j);
+
+    for (int i = -100; i < 100; ++i)
+        for (int j = 10000000; j > (10000000 - 100); --j)
+            do_test(i, j);
+
+    for (int i = -10000000; i < (-10000000 + 100); ++i)
+        for (int j = 10000000; j > (10000000 - 100); --j)
+            do_test(i, j);
+
+    for (int i = 10000000; i > (10000000 - 100); --i)
+        for (int j = 10000000; j > (10000000 - 100); --j)
+            do_test(i, j);
+}

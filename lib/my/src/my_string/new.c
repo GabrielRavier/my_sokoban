b326f7142246319c3d5e773c7648641e70b60104
Gabriel Ravier <gabravier@gmail.com>
2020-10-29 10:23:48 +0100

    Added assert for NULL self for safety
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- lib/my/src/my_string/new.c
+++ lib/my/src/my_string/new.c
@@ -7,6 +7,7 @@
 
 #include "my/my_string.h"
 #include "my/internal/my_string.h"
+#include "my/assert.h"
 #include <stdlib.h>
 
 struct my_string *my_string_new(void)
@@ -14,6 +15,7 @@ struct my_string *my_string_new(void)
     struct my_string *self =
         (struct my_string *)malloc(sizeof(struct my_string));
 
+    MY_ASSERT(self != NULL);
     self->length = 0;
     self->allocated_size = 0;
     self->string = NULL;

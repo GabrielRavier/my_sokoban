b971915b4dce2eaf40511da5e3323edfbaeb5d4d
Gabriel Ravier <gabravier@gmail.com>
2020-11-03 15:51:27 +0100

    Moved MY_COUNTOF to MY_ARRAY_SIZE and made it more robust
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- lib/my/include/my/macros.h
+++ lib/my/include/my/macros.h
@@ -7,8 +7,26 @@
 
 #pragma once
 
-// ONLY USE THIS MACRO WITH ARRAYS, NEVER WITH POINTERS
-#define MY_COUNTOF(array) ((sizeof(array)) / (sizeof((array)[0])))
+// Produces a build error if expression is non-zero, but still allows usage in
+// constant expressions
+#define MY_BUILD_BUG_ON_ZERO(expression) \
+    ((int)(sizeof(struct { int : (-!!(expression)); })))
 
+// Determines whether two expressions have the same type, though it does not
+// determine whether they have different qualifiers
+#define MY_IS_SAME_TYPE(a, b) __builtin_types_compatible(typeof(a), typeof(b))
+
+// Fails to compile if the passed expression is not an array
+#define MY_BUILD_BUG_ON_NON_ARRAY(array) \
+    MY_BUILD_BUG_ON_ZERO(MY_IS_SAME_TYPE((array), &(array)[0]))
+
+// Determines the amount of elements in an array. Will fail to compile on things
+// that aren't arrays
+#define MY_ARRAY_SIZE(array) \
+    (sizeof(array) / sizeof((array)[0]) + MY_BUILD_BUG_ON_NON_ARRAY(array))
+
+// Returns the maximum of a and b
 #define MY_MAX(a, b) ((a) > (b) ? (a) : (b))
+
+// Returns the minimum of a and b
 #define MY_MIN(a, b) ((a) > (b) ? (b) : (a))

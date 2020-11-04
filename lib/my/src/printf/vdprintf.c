bae991209c1c0628220aae2a28171f3b1482b2e1
Gabriel Ravier <gabravier@gmail.com>
2020-11-04 18:12:21 +0100

    Fixed memory leak in vdprintf
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- lib/my/src/printf/vdprintf.c
+++ lib/my/src/printf/vdprintf.c
@@ -6,14 +6,18 @@
 */
 
 #include "my/stdio.h"
+#include <stdlib.h>
 #include <unistd.h>
 
 int my_vdprintf(int fd, const char *format, va_list arguments)
 {
     char *string_to_print;
     int string_length = my_vasprintf(&string_to_print, format, arguments);
+    int result;
 
     if (string_length < 0)
         return -1;
-    return write(fd, string_to_print, string_length);
+    result = write(fd, string_to_print, string_length);
+    free(string_to_print);
+    return result;
 }

393486ff798e22ef39b7054e17fc5013d8def4c7
Gabriel Ravier <gabravier@gmail.com>
2020-11-04 22:01:03 +0100

    Fixed printing of INT_MIN
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- lib/my/src/printf/formatter/signed_integer.c
+++ lib/my/src/printf/formatter/signed_integer.c
@@ -8,17 +8,21 @@
 #include "my/internal/printf/formatter.h"
 #include <limits.h>
 
+// Note: The cast to uintptr_t (done explicitly here for clarity) will handle
+// the issue of INTMAX_MIN, since casting that to uintptr_t on 2s complement
+// (now mandatory in C2x) will result in the corresponding unsigned value
 void asprintf_format_signed_integer(struct my_string *destination,
     va_list arguments, const struct my_printf_conversion_info *format_info)
 {
-    int signed_argument = va_arg(arguments, int);
+    intptr_t signed_argument = va_arg(arguments, int);
 
     if (signed_argument < 0) {
         my_string_append_char(destination, '-');
-        if (signed_argument != INT_MAX)
+        if (signed_argument != INTMAX_MIN)
             signed_argument = -signed_argument;
     }
-    asprintf_append_number_base(destination, signed_argument, 10, false);
+    asprintf_append_number_base(destination, (uintptr_t)signed_argument, 10,
+        false);
 }
 
 
--- tests/libmy/printf/printf.c
+++ tests/libmy/printf/printf.c
@@ -9,9 +9,11 @@
 #include <criterion/criterion.h>
 #include <criterion/redirect.h>
 #include "my/stdio.h"
+#include "my/macros.h"
 #include <stdio.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <limits.h>
 
 MY_ATTRIBUTE((format(printf, 1, 2))) static void compare_printfs(const char *format, ...)
 {
@@ -85,6 +87,34 @@ Test(my_printf, format_precision_string)
 Test(my_printf, too_many_args)
 {
     compare_printfs("%d %d", 123, 456, 789);
+}
+
+Test(my_printf, through_int_checks)
+{
+    const int values[] = {INT_MIN, -17, -1, 0, 1, 17, 4711, 65535, INT_MAX};
+
+    for (int i = 0; i < MY_ARRAY_SIZE(values); ++i) {
+        compare_printfs("%d", values[i]);
+        compare_printfs("%x", values[i]);
+        compare_printfs("%X", values[i]);
+        compare_printfs("%o", values[i]);
+        compare_printfs("%#x", values[i]);
+        compare_printfs("%#X", values[i]);
+        compare_printfs("%10d", values[i]);
+        compare_printfs("%10x", values[i]);
+        compare_printfs("%10X", values[i]);
+        compare_printfs("%10o", values[i]);
+        compare_printfs("%#10x", values[i]);
+        compare_printfs("%#10X", values[i]);
+        compare_printfs("%#10o", values[i]);
+        compare_printfs("%-10d", values[i]);
+        compare_printfs("%-10x", values[i]);
+        compare_printfs("%-10X", values[i]);
+        compare_printfs("%-10o", values[i]);
+        compare_printfs("%-#10x", values[i]);
+        compare_printfs("%-#10X", values[i]);
+        compare_printfs("%-#10o", values[i]);
+    }
 }
 
 Test(my_printf, format_epitech_extension_capital_s)

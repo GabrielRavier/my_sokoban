172273e48737b3044c2e6ad44b2964a771268fb9
Gabriel Ravier <gabravier@gmail.com>
2020-11-04 22:28:59 +0100

    Did handling of hash flag for hexadecimal numbers
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- lib/my/src/printf/formatter/unsigned_integer.c
+++ lib/my/src/printf/formatter/unsigned_integer.c
@@ -10,12 +10,16 @@
 void asprintf_format_unsigned_integer(struct my_string *destination,
     va_list arguments, struct my_printf_conversion_info *format_info)
 {
-    unsigned unsigned_argument = va_arg(arguments, unsigned);
+    uintptr_t unsigned_argument = va_arg(arguments, unsigned);
+    int base = (format_info->conversion_specifier == 'b' ? 2 :
+        (format_info->conversion_specifier == 'o' ? 8 :
+        ((format_info->conversion_specifier == 'x' ||
+            format_info->conversion_specifier == 'X') ? 16 : 10)));
 
-    asprintf_append_number_base(destination,
-        unsigned_argument, (format_info->conversion_specifier == 'b' ? 2 :
-            (format_info->conversion_specifier == 'o' ? 8 :
-            ((format_info->conversion_specifier == 'x' ||
-                format_info->conversion_specifier == 'X') ? 16 : 10))),
+    if (format_info->flag_hash && base == 16 && unsigned_argument) {
+        my_string_append_char(destination, '0');
+        my_string_append_char(destination, format_info->conversion_specifier);
+    }
+    asprintf_append_number_base(destination, unsigned_argument, base,
         format_info->conversion_specifier == 'X');
 }

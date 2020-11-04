66e360c781305a2883ce72429a56caf9aff40e4a
Gabriel Ravier <gabravier@gmail.com>
2020-11-04 19:20:23 +0100

    Implemented precision in cstring printing
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- lib/my/src/printf/formatter/cstring.c
+++ lib/my/src/printf/formatter/cstring.c
@@ -13,17 +13,15 @@ void asprintf_format_cstring(struct my_string *destination, va_list arguments,
     const struct my_printf_conversion_info *format_info)
 {
     const char *string_argument = va_arg(arguments, const char *);
-    if (format_info->conversion_specifier == 's')
-        my_string_append(destination, string_argument,
-                         my_strlen(string_argument));
-    else {
-        for (size_t i = 0; string_argument[i] != '\0'; ++i)
-            if (my_isprint(string_argument[i]))
-                my_string_append_char(destination, string_argument[i]);
-            else {
-                my_string_append_char(destination, '\\');
-                asprintf_append_number_base(destination,
-                    (unsigned char)string_argument[i], 8, false);
-            }
-    }
+
+    for (size_t i = 0; string_argument[i] != '\0' &&
+            i < (size_t)format_info->precision; ++i)
+        if ((format_info->conversion_specifier == 's') ||
+            (my_isprint(string_argument[i])))
+            my_string_append_char(destination, string_argument[i]);
+        else {
+            my_string_append_char(destination, '\\');
+            asprintf_append_number_base(destination,
+                (unsigned char)string_argument[i], 8, false);
+        }
 }
--- tests/libmy/printf/printf.c
+++ tests/libmy/printf/printf.c
@@ -10,72 +10,78 @@
 #include "my/stdio.h"
 #include <unistd.h>
 
-static void redirect_std_streams(void)
+static void redirect_stdout(void)
 {
     cr_redirect_stdout();
-    cr_redirect_stderr();
 }
 
-Test(my_printf, simple_string, .init = redirect_std_streams)
+Test(my_printf, simple_string, .init = redirect_stdout)
 {
     my_printf("Hello world");
     cr_assert_stdout_eq_str("Hello world");
 }
 
-Test(my_printf, format_s, .init = redirect_std_streams)
+Test(my_printf, format_s, .init = redirect_stdout)
 {
     my_printf("%s", "string");
     cr_assert_stdout_eq_str("string");
 }
 
-Test(my_printf, format_decimal, .init = redirect_std_streams)
+Test(my_printf, format_decimal, .init = redirect_stdout)
 {
     my_printf("%d", 1239);
     my_printf("%d", -1239);
     cr_assert_stdout_eq_str("1239-1239");
 }
 
-Test(my_printf, format_integer, .init = redirect_std_streams)
+Test(my_printf, format_integer, .init = redirect_stdout)
 {
     my_printf("%i", 1239);
     my_printf("%i", -1239);
     cr_assert_stdout_eq_str("1239-1239");
 }
 
-Test(my_printf, format_unsigned, .init = redirect_std_streams)
+Test(my_printf, format_unsigned, .init = redirect_stdout)
 {
     my_printf("%u", 1239);
     cr_assert_stdout_eq_str("1239");
 }
 
-Test(my_printf, format_octal, .init = redirect_std_streams)
+Test(my_printf, format_octal, .init = redirect_stdout)
 {
     my_printf("%o", 01123);
     cr_assert_stdout_eq_str("1123");
 }
 
-Test(my_printf, format_hex_lowercase, .init = redirect_std_streams)
+Test(my_printf, format_hex_lowercase, .init = redirect_stdout)
 {
     my_printf("%x", 0x1234);
     my_printf("%x", 0xabcd);
     cr_assert_stdout_eq_str("1234abcd");
 }
 
-Test(my_printf, format_hex_uppercase, .init = redirect_std_streams)
+Test(my_printf, format_hex_uppercase, .init = redirect_stdout)
 {
     my_printf("%X", 0x1234);
     my_printf("%X", 0xABCD);
     cr_assert_stdout_eq_str("1234ABCD");
 }
 
-Test(my_printf, format_epitech_extension_capital_s, .init = redirect_std_streams)
+Test(my_printf, format_epitech_extension_capital_s, .init = redirect_stdout)
 {
     my_printf("%S", "a\002\377b");
     cr_assert_stdout_eq_str("a\\2\\377b");
 }
 
-Test(my_printf, format_epitech_extension_b, .init = redirect_std_streams)
+Test(my_printf, format_epitech_extension_b, .init = redirect_stdout)
 {
     my_printf("%b", 022645);
     cr_assert_stdout_eq_str("10010110100101");
+}
+
+Test(my_printf, format_precision_string, .init = redirect_stdout)
+{
+    const char array[5] = {'a', 'b', 'c', 'n', 'o'};
+    my_printf("%.*s", 3, array);
+    cr_assert_stdout_eq_str("abc");
 }

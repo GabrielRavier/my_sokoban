0ffd9fdf790023610cefa283ce38662f1791ef7b
Gabriel Ravier <gabravier@gmail.com>
2020-11-04 21:47:47 +0100

    Converted printf tests to use more convenient framework
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- tests/libmy/printf/printf.c
+++ tests/libmy/printf/printf.c
@@ -5,83 +5,98 @@
 ** Tests printf
 */
 
+#define _GNU_SOURCE
 #include <criterion/criterion.h>
 #include <criterion/redirect.h>
 #include "my/stdio.h"
+#include <stdio.h>
 #include <unistd.h>
+#include <stdbool.h>
 
-static void redirect_stdout(void)
+MY_ATTRIBUTE((format(printf, 1, 2))) static void compare_printfs(const char *format, ...)
 {
     cr_redirect_stdout();
+
+    va_list arguments;
+    char *result_us, *result_libc;
+
+    va_start(arguments, format);
+    my_vprintf(format, arguments);
+    va_end(arguments);
+
+    va_start(arguments, format);
+    my_vasprintf(&result_us, format, arguments);
+    va_end(arguments);
+
+    va_start(arguments, format);
+    vasprintf(&result_libc, format, arguments);
+    va_end(arguments);
+
+    cr_assert_str_eq(result_us, result_libc);
+    cr_assert_stdout_eq_str(result_libc);
 }
 
-Test(my_printf, simple_string, .init = redirect_stdout)
+Test(my_printf, simple_string)
 {
-    my_printf("Hello world");
-    cr_assert_stdout_eq_str("Hello world");
+    compare_printfs("Hello world");
 }
 
-Test(my_printf, format_s, .init = redirect_stdout)
+Test(my_printf, format_s)
 {
-    my_printf("%s", "string");
-    cr_assert_stdout_eq_str("string");
+    compare_printfs("%s", "string");
 }
 
-Test(my_printf, format_decimal, .init = redirect_stdout)
+Test(my_printf, format_decimal)
 {
-    my_printf("%d", 1239);
-    my_printf("%d", -1239);
-    cr_assert_stdout_eq_str("1239-1239");
+    compare_printfs("%d %d", 1239, -1239);
 }
 
-Test(my_printf, format_integer, .init = redirect_stdout)
+Test(my_printf, format_integer)
 {
-    my_printf("%i", 1239);
-    my_printf("%i", -1239);
-    cr_assert_stdout_eq_str("1239-1239");
+    compare_printfs("%i %i", 1239, -1239);
 }
 
-Test(my_printf, format_unsigned, .init = redirect_stdout)
+Test(my_printf, format_unsigned)
 {
-    my_printf("%u", 1239);
-    cr_assert_stdout_eq_str("1239");
+    compare_printfs("%u", 1239);
 }
 
-Test(my_printf, format_octal, .init = redirect_stdout)
+Test(my_printf, format_octal)
 {
-    my_printf("%o", 01123);
-    cr_assert_stdout_eq_str("1123");
+    compare_printfs("%o", 01123);
 }
 
-Test(my_printf, format_hex_lowercase, .init = redirect_stdout)
+Test(my_printf, format_hex_lowercase)
 {
-    my_printf("%x", 0x1234);
-    my_printf("%x", 0xabcd);
-    cr_assert_stdout_eq_str("1234abcd");
+    compare_printfs("%x %x", 0x1234, 0xabcd);
 }
 
-Test(my_printf, format_hex_uppercase, .init = redirect_stdout)
+Test(my_printf, format_hex_uppercase)
 {
-    my_printf("%X", 0x1234);
-    my_printf("%X", 0xABCD);
-    cr_assert_stdout_eq_str("1234ABCD");
+    compare_printfs("%X %X", 0x1234, 0xABCD);
 }
 
-Test(my_printf, format_epitech_extension_capital_s, .init = redirect_stdout)
+Test(my_printf, format_precision_string)
 {
+    const char array[5] = {'a', 'b', 'c', 'n', 'o'};
+    compare_printfs("%.*s", 3, array);
+}
+
+Test(my_printf, too_many_args)
+{
+    compare_printfs("%d %d", 123, 456, 789);
+}
+
+Test(my_printf, format_epitech_extension_capital_s)
+{
+    cr_redirect_stdout();
     my_printf("%S", "a\002\377b");
     cr_assert_stdout_eq_str("a\\2\\377b");
 }
 
-Test(my_printf, format_epitech_extension_b, .init = redirect_stdout)
+Test(my_printf, format_epitech_extension_b)
 {
+    cr_redirect_stdout();
     my_printf("%b", 022645);
     cr_assert_stdout_eq_str("10010110100101");
-}
-
-Test(my_printf, format_precision_string, .init = redirect_stdout)
-{
-    const char array[5] = {'a', 'b', 'c', 'n', 'o'};
-    my_printf("%.*s", 3, array);
-    cr_assert_stdout_eq_str("abc");
 }

8d26a358d9735fdcff679944c8b0fa45dffa0334
Gabriel Ravier <gabravier@gmail.com>
2020-11-05 18:47:24 +0100

    Added more floating point checks
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- tests/libmy/printf/printf.c
+++ tests/libmy/printf/printf.c
@@ -363,8 +363,7 @@ Test(my_printf, through_size_t_checks, .init = cr_redirect_stdout, .fini = compa
 
 Test(my_printf, through_float_checks, .init = cr_redirect_stdout, .fini = compare_all_libc_to_stdout)
 {
-    static const double values[] = {-(0.0 / 0.0), -99999, -99, -17.4, -4.3, -3.0, -1.5, -1, 0, 0.1, 0.2342374852, 0.2340007,
-        3.1415926, 14.7845, 34.24758, 9999, 9999999, (0.0 / 0.0)};
+    static const double values[] = {-(0.0 / 0.0), -99999, -99, -17.4, -4.3, -3.0, -1.5, -1, 0, 0.1, 0.2342374852, 0.2340007, 3.1415926, 14.7845, 34.24758, 9999, 9999999, (0.0 / 0.0), 0.001, 1.0e-20, 1.0, 100.0, 9.9999, -0.00543, -99.99999};
 
     for (size_t i = 0; i < MY_ARRAY_SIZE(values); ++i) {
         compare_printfs("%f", values[i]);
@@ -404,6 +403,22 @@ Test(my_printf, through_float_checks, .init = cr_redirect_stdout, .fini = compar
         compare_printfs("%5.2G", values[i]);
         compare_printfs("%0G", values[i]);
         compare_printfs("%#G", values[i]);
+    }
+}
+
+Test(my_printf, through_float_widths_checks, .init = cr_redirect_stdout, .fini = compare_all_libc_to_stdout)
+{
+    static const double values[] = {-(0.0 / 0.0), -99999, -99, -17.4, -4.3, -3.0, -1.5, -1, 0, 0.1, 0.2342374852, 0.2340007, 3.1415926, 14.7845, 34.24758, 9999, 9999999, (0.0 / 0.0), 0.001, 1.0e-20, 1.0, 100.0, 9.9999, -0.00543, -99.99999};
+    static const int lengths[] = {1, 1, 5, 5, 10, 10, 5, 1};
+
+    for (size_t i = 0; i < (MY_ARRAY_SIZE(lengths) - 1); ++i) {
+        for (size_t j = 0; j < MY_ARRAY_SIZE(values); ++j) {
+            compare_printfs("%*.*f", lengths[i], lengths[i + 1], values[j]);
+            compare_printfs("%*.*e", lengths[i], lengths[i + 1], values[j]);
+            compare_printfs("%*.*E", lengths[i], lengths[i + 1], values[j]);
+            compare_printfs("%*.*g", lengths[i], lengths[i + 1], values[j]);
+            compare_printfs("%*.*G", lengths[i], lengths[i + 1], values[j]);
+        }
     }
 }
 

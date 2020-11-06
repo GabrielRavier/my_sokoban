ecd415873ae7340cf6d9f836a10c809be29d6001
Gabriel Ravier <gabravier@gmail.com>
2020-11-06 09:42:44 +0100

    Added handling for NULL pointers in %p according to glibc
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- lib/my/src/printf/formatter/pointer.c
+++ lib/my/src/printf/formatter/pointer.c
@@ -11,8 +11,13 @@ struct my_string *asprintf_format_pointer(struct my_string *destination,
     va_list arguments, struct my_printf_conversion_info *format_info)
 {
     uintptr_t argument = (uintptr_t)va_arg(arguments, void *);
-    struct my_string *prefix = my_string_new_from_string("0x", 2);
+    struct my_string *prefix = NULL;
 
-    asprintf_append_number_base(destination, argument, 16, false);
+    if (argument) {
+        prefix = my_string_new_from_string("0x", 2);
+        asprintf_append_number_base(destination, argument, 16, false);
+    }
+    else
+        my_string_append(destination, "(nil)", 5);
     return prefix;
 }

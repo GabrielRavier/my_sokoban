ac5dda5e1dacc727b0cdebef0e7635f8641e639f
Gabriel Ravier <gabravier@gmail.com>
2020-11-12 14:24:48 +0100

    Correct C1 violation in stdio/printf/parse_conversion_info/parse_int
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- lib/my/src/stdio/printf/parse_conversion_info/parse_int.c
+++ lib/my/src/stdio/printf/parse_conversion_info/parse_int.c
@@ -9,20 +9,29 @@
 #include "my/ctype.h"
 #include <limits.h>
 
+static void do_loop_iter(int *result, char digit)
+{
+    if (*result > (INT_MAX / 10)) {
+        *result = -1;
+        return;
+    }
+    *result *= 10;
+    if (INT_MAX - digit < *result) {
+        *result = -1;
+        return;
+    }
+    *result += digit;
+}
+
 int asprintf_parse_int(const char **to_parse)
 {
     int result = **to_parse - '0';
-    char digit;
 
-    while (my_isdigit(*++*to_parse))
-        if (result >= 0) {
-            if (result > (INT_MAX / 10))
-                return -1;
-            digit = **to_parse - '0';
-            result *= 10;
-            if (INT_MAX - digit < result)
-                return -1;
-            result += digit;
-        }
+    while (my_isdigit(*++*to_parse)) {
+        if (result >= 0)
+            do_loop_iter(&result, **to_parse - '0');
+        if (result < 0)
+            break;
+    }
     return result;
 }

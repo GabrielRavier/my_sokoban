7bab8b5072e87edc7257d5ee7120b9a5c93ad397
Gabriel Ravier <gabravier@gmail.com>
2020-11-21 18:53:33 +0100

    libmy: Corrected spacing in strcasecmp
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- lib/my/src/strings/strcasecmp.c
+++ lib/my/src/strings/strcasecmp.c
@@ -10,7 +10,7 @@
 #include <stddef.h>
 
 int my_strcasecmp(const char *s1, const char *s2)
-{    
+{
     for (size_t i = 0;; ++i) {
         if (my_tolower(s1[i]) != my_tolower(s2[i]))
             return my_tolower(s1[i]) - my_tolower(s2[i]);

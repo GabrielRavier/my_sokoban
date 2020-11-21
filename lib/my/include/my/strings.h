f8781f7bb579fb3c11d94ef12f1953db9ff22851
Gabriel Ravier <gabravier@gmail.com>
2020-11-21 12:24:52 +0100

    Fixed a bunch of stuff from mouli
    Fixed:
    - Sorting, now is done correctly through a caseless comparison
    - Printing the sticky bit
    - Printing the device id for block/character devices
    - Printing the total blocks
    
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

---
+++ lib/my/include/my/strings.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2020
+** libmy
+** File description:
+** Implements strings.h
+*/
+
+#pragma once
+
+#include "my/config.h"
+
+int my_strcasecmp(const char *s1, const char *s2) MY_ATTR_NOTHROW MY_ATTR_PURE
+    MY_ATTR_NONNULL((1, 2));
---
+++ lib/my/src/strings/strcasecmp.c
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2020
+** libmy
+** File description:
+** Implements strcasecmp
+*/
+
+#include "my/strings.h"
+#include "my/ctype.h"
+#include <stddef.h>
+
+int my_strcasecmp(const char *s1, const char *s2)
+{    
+    for (size_t i = 0;; ++i) {
+        if (my_tolower(s1[i]) != my_tolower(s2[i]))
+            return my_tolower(s1[i]) - my_tolower(s2[i]);
+        if (s1[i] == '\0')
+            return (0);
+    }    
+}

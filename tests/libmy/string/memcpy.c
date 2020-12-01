cfac47cfd85f6f9b25dab7aff7651b522813e076
Gabriel Ravier <gabravier@gmail.com>
2020-12-01 11:21:57 +0100

    Added more memmove tests
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- tests/libmy/string/memcpy.c
+++ tests/libmy/string/memcpy.c
@@ -10,15 +10,6 @@
 #include <string.h>
 #include <stdint.h>
 
-#define BUFFER_SIZE 10240
-#define START_COPY 2560
-#define MAX_BLOCK_SIZE 256
-#define MAX_OFFSET 3
-
-#if (START_COPY + MAX_OFFSET + MAX_BLOCK_SIZE >= BUFFER_SIZE)
-    #error "Won't compile, there would be a buffer overrun"
-#endif
-
 inline static uint32_t msws() {
     static const uint64_t s = 0xb5ad4eceda1ce2a9;
     static uint64_t x = 0, w = 0;
@@ -29,10 +20,13 @@ inline static uint32_t msws() {
 
 Test(my_memcpy, buffers)
 {
+    static const size_t BUFFER_SIZE = 10240, START_COPY = 2560, MAX_BLOCK_SIZE = 256, MAX_OFFSET = 3;
     char source[BUFFER_SIZE];
     char destination[BUFFER_SIZE];
     char backup_source[BUFFER_SIZE];
     size_t i;
+
+    cr_assert_not(START_COPY + MAX_OFFSET + MAX_BLOCK_SIZE >= BUFFER_SIZE);
 
     for (i = 0; i < BUFFER_SIZE; ++i)
         backup_source[i] = source[i] = msws() | 1;

57194e7015c18ffdd5d2a57fb0f51c4aa8bc27be
Gabriel Ravier <gabravier@gmail.com>
2020-12-24 00:53:35 +0100

    Slightly improved memchr performance
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- lib/my/src/string/memchr_x86_64.s
+++ lib/my/src/string/memchr_x86_64.s
@@ -6,79 +6,93 @@
     .p2align 4
 my_memchr:
     .cfi_startproc
-    mov rax, rdi
-    test al, 7
-    je .Laligned
 
-    lea rcx, [rax + 1]
+    mov rax, rdi
+    movzx r10d, sil
+    test al, 7
+    jne .Lstart_align_loop
+    jmp .Ldo_aligned
 
     .p2align 4
 .Lalign_loop:
-    # Use sub so that jb works
-    sub rdx, 1
-    jb .Lreturn_0
+    movzx ecx, byte ptr [rax]
+    cmp ecx, r10d
+    je .Lstart_trailing_bytes_loop
 
-    cmp byte ptr [rax], sil
-    je .Lreturn
-
-    mov rdi, rcx
     inc rax
-    inc rdi
-    test cl, 7
-    mov rcx, rdi
+    dec rdx
+    test al, 7
+    je .Ldo_aligned
+
+.Lstart_align_loop:
+    test rdx, rdx
     jne .Lalign_loop
 
-.Laligned:
-    cmp rdx, 8
-    jb .Lstart_trailing_bytes_loop
+.Lreturn_0:
+    xor r8d, r8d
 
-    movzx ecx, sil
-    movabs r8, 0x7F7F7F7F7F7F7F80
-    movabs r9, -0x101010101010101
-    mov edi, ecx
-    shl edi, 8
-    or edi, ecx
-    mov rcx, rdi
-    shl rcx, 16
-    or rcx, rdi
-    mov r10, rcx
-    shl r10, 32
-    or r10, rcx
+.Lreturn_r8:
+    mov rax, r8
+    ret
 
     .p2align 4
-.Laligned_loop:
-    mov rcx, [rax]
-    xor rcx, r10
-    mov rdi, rcx
-    add rcx, r9
-    not rdi
-    and rdi, r8
-    test rdi, rcx
-    jne .Lstart_trailing_bytes_loop
 
-    add rdx, -8
-    add rax, 8
+.Ldo_aligned:
+    xor r8d, r8d
+    test rdx, rdx
+    je .Lreturn_r8
+
+    movzx ecx, byte ptr [rax]
+    cmp ecx, r10d
+    je .Lstart_trailing_bytes_loop
+
+    movsx rdi, r10d
+    movabs rcx, 0x101010101010101
+    movabs r9, -0x101010101010101
+    movabs r8, -0x7F7F7F7F7F7F7F80
+    imul rdi, rcx
     cmp rdx, 7
-    ja .Laligned_loop
+    ja .Lstart_aligned_loop
     jmp .Lstart_trailing_bytes_loop
 
     .p2align 4
-.Ltrailing_bytes_loop:
-    cmp [rax], sil
-    je .Lreturn
 
-    dec rdx
-    inc rax
+.Laligned_loop:
+    sub rdx, 8
+    add rax, 8
+    cmp rdx, 7
+    jbe .Lless_than_8
+
+.Lstart_aligned_loop:
+    mov rcx, [rax]
+    xor rcx, rdi
+    lea rsi, [rcx + r9]
+    not rcx
+    and rcx, rsi
+    test rcx, r8
+    je .Laligned_loop
 
 .Lstart_trailing_bytes_loop:
-    test rdx, rdx
+    mov r8, rax
+    add rax, rdx
+
+    .p2align 4
+
+.Ltrailing_bytes_loop:
+    movzx edx, byte ptr [r8]
+    cmp edx, r10d
+    je .Lreturn_r8
+
+    inc r8
+    cmp r8, rax
     jne .Ltrailing_bytes_loop
+    jmp .Lreturn_0
 
-.Lreturn_0:
-    xor eax, eax
-
-.Lreturn:
-    ret
+    .p2align 4
+.Lless_than_8:
+    test rdx, rdx
+    je .Lreturn_0
+    jmp .Lstart_trailing_bytes_loop
 
     .cfi_endproc
     .size my_memchr, . - my_memchr

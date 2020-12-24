0bbdebfbfa9f1c07ca0c724029b9b0f8cbaaff54
Gabriel Ravier <gabravier@gmail.com>
2020-12-24 01:40:06 +0100

    libmy: Moved memchr to an SSE2-based implementation that should be mildly faster
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- lib/my/src/string/memchr_x86_64.s
+++ lib/my/src/string/memchr_x86_64.s
@@ -7,92 +7,49 @@
 my_memchr:
     .cfi_startproc
 
+    mov rcx, rdi
+    movzx esi, sil
+    test rdx, rdx
+    je .Lreturn_0
+
+    imul esi, esi, 0x1010101
+    and ecx, 0xF
+    mov r8d, -1
     mov rax, rdi
-    movzx r10d, sil
-    test al, 7
-    jne .Lstart_align_loop
-    jmp .Ldo_aligned
+    sal r8d, cl
+    add rdx, rdi
+    and rax, -0x10
+    mov ecx, r8d
+    movd xmm2, esi
+    pshufd xmm1, xmm2, 0
+    jmp .Lstart_loop
 
     .p2align 4
-.Lalign_loop:
-    movzx ecx, byte ptr [rax]
-    cmp ecx, r10d
-    je .Lstart_trailing_bytes_loop
+.Lloop:
+    add rax, 0x10
+    mov ecx, -1
+    cmp rax, rdx
+    jnb .Lreturn_0
 
-    inc rax
-    dec rdx
-    test al, 7
-    je .Ldo_aligned
+.Lstart_loop:
+    movdqa xmm0, xmm1
+    pcmpeqb xmm0, [rax]
+    pmovmskb esi, xmm0
+    and ecx, esi
+    je .Lloop
 
-.Lstart_align_loop:
-    test rdx, rdx
-    jne .Lalign_loop
+    bsf ecx, ecx
+    add rax, rcx
+    cmp rax, rdx
+    jnb .Lreturn_0
 
-.Lreturn_0:
-    xor r8d, r8d
-
-.Lreturn_r8:
-    mov rax, r8
     ret
 
     .p2align 4
 
-.Ldo_aligned:
-    xor r8d, r8d
-    test rdx, rdx
-    je .Lreturn_r8
-
-    movzx ecx, byte ptr [rax]
-    cmp ecx, r10d
-    je .Lstart_trailing_bytes_loop
-
-    movsx rdi, r10d
-    movabs rcx, 0x101010101010101
-    movabs r9, -0x101010101010101
-    movabs r8, -0x7F7F7F7F7F7F7F80
-    imul rdi, rcx
-    cmp rdx, 7
-    ja .Lstart_aligned_loop
-    jmp .Lstart_trailing_bytes_loop
-
-    .p2align 4
-
-.Laligned_loop:
-    sub rdx, 8
-    add rax, 8
-    cmp rdx, 7
-    jbe .Lless_than_8
-
-.Lstart_aligned_loop:
-    mov rcx, [rax]
-    xor rcx, rdi
-    lea rsi, [rcx + r9]
-    not rcx
-    and rcx, rsi
-    test rcx, r8
-    je .Laligned_loop
-
-.Lstart_trailing_bytes_loop:
-    mov r8, rax
-    add rax, rdx
-
-    .p2align 4
-
-.Ltrailing_bytes_loop:
-    movzx edx, byte ptr [r8]
-    cmp edx, r10d
-    je .Lreturn_r8
-
-    inc r8
-    cmp r8, rax
-    jne .Ltrailing_bytes_loop
-    jmp .Lreturn_0
-
-    .p2align 4
-.Lless_than_8:
-    test rdx, rdx
-    je .Lreturn_0
-    jmp .Lstart_trailing_bytes_loop
+.Lreturn_0:
+    xor eax, eax
+    ret
 
     .cfi_endproc
     .size my_memchr, . - my_memchr

3d6ef07733bda0927c33e5d92aca66519f532ca0
Gabriel Ravier <gabravier@gmail.com>
2021-01-10 19:54:44 +0100

    libmy: Fixed MY_REPLACE formatting
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- lib/my/include/my/algorithm.h
+++ lib/my/include/my/algorithm.h
@@ -11,8 +11,8 @@
 // [first, last]
 #define MY_REPLACE(first_param, last, old_value, new_value) \
     do { \
-    __auto_type first = (first_param); \
-    for (; first != (last); ++first) \
-        if (*first == (old_value)) \
-            *first = (new_value); \
+        __auto_type first = (first_param); \
+        for (; first != (last); ++first) \
+            if (*first == (old_value)) \
+                *first = (new_value); \
     } while (0)

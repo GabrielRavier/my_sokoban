7c5c0111a2c85f0f0d36d62e7097c5d29200fa81
Gabriel Ravier <gabravier@gmail.com>
2021-01-13 00:53:18 +0100

    Made functions from float_bit_equality static inline
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- tests/libmy/math/float_bit_equality.h
+++ tests/libmy/math/float_bit_equality.h
@@ -9,17 +9,17 @@
 
 #include "my/internal/math.h"
 
-bool float_bit_eq(float a, float b)
+static inline bool float_bit_eq(float a, float b)
 {
     return ((union my_ieee754_float_parts){.as_float = a}).as_u32 == ((union my_ieee754_float_parts){.as_float = b}).as_u32;
 }
 
-bool double_bit_eq(double a, double b)
+static inline bool double_bit_eq(double a, double b)
 {
     return ((union my_ieee754_double_parts){.as_double = a}).as_u64 == ((union my_ieee754_double_parts){.as_double = b}).as_u64;
 }
 
-bool long_double_bit_eq(long double a, long double b)
+static inline bool long_double_bit_eq(long double a, long double b)
 {
     union my_ieee754_long_double_parts a_u = {.as_long_double = a};
     union my_ieee754_long_double_parts b_u = {.as_long_double = b};

c326bfd7ea38d4811ab4f01638e9a1326a83db8c
Gabriel Ravier <gabravier@gmail.com>
2021-01-14 01:26:41 +0100

    Fix discrepencies with day 09 libmy
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

---
+++ lib/my/src/my_strcat.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2020
+** libmy
+** File description:
+** Appends src to dest
+*/
+
+#include "my.h"
+
+char *my_strcat(char *destination, const char *source)
+{
+    my_strcpy(destination + my_strlen(destination), source);
+    return destination;
+}
---
+++ lib/my/src/my_strncat.c
@@ -0,0 +1,32 @@
+/*
+** EPITECH PROJECT, 2020
+** libmy
+** File description:
+** Appends src to dest, using at most n bytes from src
+*/
+
+#include "my.h"
+#include <stdbool.h>
+
+char *my_strncat(char *destination, const char *source, int n)
+{
+    char *result = destination;
+    char current_character;
+
+    destination += my_strlen(destination);
+
+    while (true) {
+        current_character = *source++;
+        *destination++ = current_character;
+
+        if (current_character == '\0')
+            break;
+
+        if (--n < 0) {
+            *--destination = '\0';
+            break;
+        }
+    }
+
+    return result;
+}

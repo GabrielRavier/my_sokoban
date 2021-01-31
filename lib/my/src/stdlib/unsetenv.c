5f47c56b8fd591848f9418b1104cb62a09e76791
Gabriel Ravier <gabravier@gmail.com>
2021-01-31 11:09:58 +0100

    libmy: Added input checking to unsetenv
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- lib/my/src/stdlib/unsetenv.c
+++ lib/my/src/stdlib/unsetenv.c
@@ -7,6 +7,8 @@
 
 #include "my/stdlib.h"
 #include "my/internal/stdlib.h"
+#include "my/string.h"
+#include <errno.h>
 
 extern char **environ;
 
@@ -15,6 +17,10 @@ int my_unsetenv(const char *name)
     char **environ_it;
     size_t offset;
 
+    if (*name == '\0' || my_strchr(name, '=') != NULL) {
+        errno = EINVAL;
+        return (-1);
+    }
     while (my_getenv_offset(name, &offset) != NULL)
         for (environ_it = &environ[offset];; ++environ_it) {
             environ_it[0] = environ_it[1];

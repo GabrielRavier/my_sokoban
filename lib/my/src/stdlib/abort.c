7da58dfd934cd9738e7d757a44457508a77a9369
Gabriel Ravier <gabravier@gmail.com>
2021-02-04 18:46:04 +0100

    libmy: Made abort loop the trap so it's guaranteed to never return

--- lib/my/src/stdlib/abort.c
+++ lib/my/src/stdlib/abort.c
@@ -7,11 +7,13 @@
 
 #include "my/stdlib.h"
 #include "my/internal/coverage_flush.h"
+#include <stdbool.h>
 
 // This uses __builtin_trap to avoid problems with restrictions (say, being
 // forbidden from using abort(3))
 void my_abort(void)
 {
     MY_COVERAGE_FLUSH();
-    __builtin_trap();
+    while (true)
+        __builtin_trap();
 }

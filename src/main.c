2d140e64fb48de3e254abaa0c3986d12bfe1c976
Gabriel Ravier <gabravier@gmail.com>
2021-02-21 11:32:11 +0100

    Used tabs instead of spaces to make the usage message look better

--- src/main.c
+++ src/main.c
@@ -13,8 +13,8 @@
 
 static void usage(const char *argv0)
 {
-    my_printf("USAGE\n     %s map\nDESCRIPTION\n     map  file representing the"
-        " warehouse map, containing '#' for walls,\n          "
+    my_printf("USAGE\n\t%s map\nDESCRIPTION\n\tmap  file representing the"
+        " warehouse map, containing '#' for walls,\n\t     "
         "'P' for the player, 'X' for boxes and 'O' for storage locations.\n",
         argv0);
 }
